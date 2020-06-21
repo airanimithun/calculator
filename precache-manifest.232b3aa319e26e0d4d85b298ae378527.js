390750ca8ec09b61f847400ebfa3326e5e1618ef
Mithun <mas@extremenetworks.com>
2020-06-21 18:16:22 +0530

    Updates

---
+++ precache-manifest.232b3aa319e26e0d4d85b298ae378527.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1634eed64d6a3948b58c0239c4d5c9a4",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "86b6a164c4dd5c1c9ac0",
+    "url": "/calculator/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "92db9637774e18b94cd4",
+    "url": "/calculator/static/js/2.fbe97ffb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator/static/js/2.fbe97ffb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86b6a164c4dd5c1c9ac0",
+    "url": "/calculator/static/js/main.c4d0b218.chunk.js"
+  },
+  {
+    "revision": "8355966709433f552c3b",
+    "url": "/calculator/static/js/runtime-main.51f5447e.js"
+  }
+]);
